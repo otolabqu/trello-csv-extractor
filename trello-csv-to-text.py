815f2adc5154b15934947831c5680c21480c51e6
otolabqu <otolabqu@users.noreply.github.com>
2023-04-07 22:42:58 +0200

    add http urls from desc. sort list. improve variable naming

--- trello-csv-to-text.py
+++ trello-csv-to-text.py
@@ -6,29 +6,35 @@
 import json
 
 with open('XPfA74Iz.json') as thefile:
-    t = thefile.read()
-    j = json.loads(t)
-    lists = {}
-    for l in j['lists']:
-        lists[l['id']] = l['name']
+    j = json.loads(thefile.read())
+    out_lists = {}
+    for in_list in j['lists']:
+        out_lists[in_list['id']] = in_list['name']
     
     print('found lists:')
-    for list_id, name in lists.items():
+    for list_id, name in out_lists.items():
         print(list_id, name)
     
     json_cards = j['cards']
 
-    for l, list_name in lists.items():
+    for list_id, list_name in out_lists.items():
         print()
         print("#############################")
         print("Starting with list: " + list_name)
         print("#############################")
         print()
+        current_list = []
         for c in json_cards:
-            if c['idList'] == l:
+            if c['idList'] == list_id:
                 name_raw = c['name']
+                desc = c['desc']
+                if desc is not None and 'http' in desc:
+                    name_raw = desc + " " + name_raw
                 # We want to keep the text on one line, not using the carriage return characters that Trello produces.
                 # Otherwise long card names with line breaks will be broken after sorting the text file.
                 # It might still be useful to be able to see where the line break was, so let's replace it.
                 name_mod = name_raw.replace('\r', '_r_')
-                print(name_mod)
+                current_list.append(name_mod)
+        current_list.sort()
+        for x in current_list:
+            print(x)
